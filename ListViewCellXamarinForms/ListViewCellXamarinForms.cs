8748405f67bc9d6bf4c02f157f736d4d56988361
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 19:09:15 -0400

    How to Create a Simple ListView App in Xamarin form

---
+++ ListViewCellXamarinForms/ListViewCellXamarinForms.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace ListViewCellXamarinForms
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			List<string> list = new List<string> ();
+			list.Add ("Apple");
+			list.Add ("Mango");
+			list.Add ("Banana");
+			list.Add ("WaterMelon");
+			list.Add ("Lemon");
+			list.Add ("Graps");
+
+			ListView listview=new ListView(){
+				ItemsSource=list };
+		
+			// The root page of your application
+			MainPage = new ContentPage 
+			{
+				Content = new StackLayout 
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = 
+					{
+				listview
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
